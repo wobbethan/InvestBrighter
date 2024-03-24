4f29d939e4c5b7403d6d2e7ed64d2ff657e5cdcb
wobbethan <wobbethan@gmail.com>
2024-03-24 14:15:12 -0400

    Working, fixed by checking if event still existed

--- frontend/src/pages/admin/AdminAllOrdersPage.jsx
+++ frontend/src/pages/admin/AdminAllOrdersPage.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import AdminHeader from "../components/Layout/AdminHeader";
 import AdminSideBar from "../components/Admin/Layout/AdminSideBar";
+import { toast } from "react-toastify";
+import { AiOutlineDelete } from "react-icons/ai";
+
 import {
   DataGrid,
   GridToolbar,
@@ -18,6 +21,9 @@ import {
 import axios from "axios";
 import { server } from "../../Server";
 import Loader from "../../components/Layout/Loader";
+import { Button } from "@material-ui/core";
+import { RxCross1 } from "react-icons/rx";
+import styles from "../../styles/styles";
 
 let options = {
   timeZone: "America/New_York",
@@ -34,21 +40,52 @@ const AdminAllOrdersPage = () => {
   const [sections, setSections] = useState([]);
   const [selectedSection, setSelectedSection] = useState("All Sections");
   const [loading, setLoading] = useState(false);
-
+  const [confirmOpen, setConfirmOpen] = useState();
+  const [orderDelete, setOrderDelete] = useState();
   const { user } = useSelector((state) => state.user);
   const { adminOrders, adminOrderLoading } = useSelector(
     (state) => state.order
   );
   const [productData, setProductData] = useState();
 
-  useEffect(() => {
-    setLoading(true);
+  const handleDelete = async () => {
+    const transfer = toast.loading("Deleting Order");
+    await axios
+      .delete(`${server}/order/admin-delete-orders/${orderDelete}`)
+      .then((res) => {
+        if (res.data.success == true) {
+          toast.update(transfer, {
+            render: "Order deleted",
+            type: "success",
+            isLoading: false,
+            autoClose: 3000,
+          });
+        } else {
+          toast.update(transfer, {
+            render: "Order deletion failed",
+            type: "error",
+            isLoading: false,
+            autoClose: 3000,
+          });
+        }
+      });
+
+    getOrders();
+
+    setConfirmOpen(false);
+  };
+
+  const getOrders = () => {
     if (selectedSection !== "All Sections") {
       dispatch(filterAllOrdersOfAdmin(selectedSection, user._id));
     } else {
       dispatch(getAllOrdersOfAdmin(user._id));
     }
+  };
 
+  useEffect(() => {
+    setLoading(true);
+    getOrders();
     setLoading(false);
   }, [selectedSection]);
 
@@ -123,6 +160,27 @@ const AdminAllOrdersPage = () => {
       minWidth: 130,
       flex: 0.8,
     },
+    {
+      field: "delete",
+      flex: 1,
+      minWidth: 75,
+      headerName: "Delete Investment",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Button
+              onClick={() =>
+                setConfirmOpen(true) || setOrderDelete(params.row.id)
+              }
+            >
+              <AiOutlineDelete size={20} />
+            </Button>
+          </>
+        );
+      },
+    },
   ];
 
   const row = [];
@@ -188,6 +246,32 @@ const AdminAllOrdersPage = () => {
           </>
         </div>
       </div>
+      {confirmOpen && (
+        <div className="w-full fixed top-0 left-0 z-[999] bg-[#00000039] flex items-center justify-center h-screen">
+          <div className="w-[95%] 800px:w-[40%] min-h-[20vh] bg-white rounded shadow p-5">
+            <div className="w-full flex justify-end cursor-pointer">
+              <RxCross1 size={25} onClick={() => setConfirmOpen(false)} />
+            </div>
+            <h3 className="text-[25px] text-center py-5 font-Poppins text-[#000000cb]">
+              Are you sure you want to delete this investment?
+            </h3>
+            <div className="w-full flex items-center justify-center">
+              <div
+                className={`${styles.button} text-white text-[18px] !h-[42px] mr-4`}
+                onClick={() => setConfirmOpen(false)}
+              >
+                cancel
+              </div>
+              <div
+                className={`${styles.button} text-white text-[18px] !h-[42px] ml-4`}
+                onClick={() => setConfirmOpen(false) || handleDelete()}
+              >
+                confirm
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
